f87829fda2d84c43e774447c0b09656493912cda
Johann Lacroix <johann.lacroix@gmail.com>
2023-12-18 12:11:26 +0100

    Route back modifier profil

--- frontend/screens/ProfilScreen.js
+++ frontend/screens/ProfilScreen.js
@@ -38,7 +38,6 @@ export default function ProfilScreen({ navigation }) {
     const token = user.token;
 
 
-
     useEffect(() => {
         fetch(`${backendAddress}/users/getProfilInfos/${token}`)
             .then(response => response.json())
@@ -113,8 +112,7 @@ export default function ProfilScreen({ navigation }) {
                                 )}
                             </View>
                             <View>
-                                <TouchableOpacity activeOpacity={0.8} style={styles.btn}>
-
+                                <TouchableOpacity onPress={() => modifyProfil()}activeOpacity={0.8} style={styles.btn}>
                                     <Text style={styles.white}>
                                         Enregistrer les modifications
                                     </Text>
