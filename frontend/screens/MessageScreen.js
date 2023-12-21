b5eb6b35ed59ce8d12cb6b7276d042c8bf6a1a02
idriss <idriss.adinani@gmai.com>
2023-12-21 17:03:25 +0100

    test pusher

--- frontend/screens/MessageScreen.js
+++ frontend/screens/MessageScreen.js
@@ -16,12 +16,12 @@ import { Ionicons } from '@expo/vector-icons';
 import { BACKEND_ADDRESS } from "@env";
 const backendAddress = BACKEND_ADDRESS;
 
-
+let pusher = null;
 export default function MessageScreen({ navigation }) {
 
     const idProduct = '658015ffc0219a07a6ef8a49';
     const imgProduct = 'https://res.cloudinary.com/dzdrlauim/image/upload/v1702893055/mfuued7dtdxewhdqmghl.jpg';
-    let pusher = null;
+
     const isFocused = useIsFocused();
     const user = useSelector((state) => state.user.value);
     // console.log(user)
